2c59e1443dba9f149cba3c14190da3ff59114285
유민호 <wingedcompany@gmail.com>
2021-09-30 23:56:18 +0900

    start 5

---
+++ src/views/Buttons.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-btn color="primary">
+        primary
+      </v-btn>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
---
+++ src/views/Icons.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container
+    fluid
+  >
+    <v-card class="pa-3">
+      Col2
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
